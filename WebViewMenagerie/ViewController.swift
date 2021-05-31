f3534d262f2574ae38aa194f43c31b20d97284fd
Haskell Pointer <haskell@saucelabs.com>
2021-05-31 13:50:49 +0200

    Initial Commit

---
+++ WebViewMenagerie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewMenagerie
+//
+//  Created by Kyle Pointer on 31.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
